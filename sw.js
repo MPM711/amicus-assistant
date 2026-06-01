8a8074fcf87adb8186aaf9565bbd42fff57029d3
MPM711 <136309302+MPM711@users.noreply.github.com>
2026-06-01 23:51:47 +0530

    Update service worker to version 5 and improve caching

--- sw.js
+++ sw.js
@@ -1,19 +1,8 @@
-// v4 - clears all caches on install
-const CACHE = 'amicus-v4';
-
-self.addEventListener('install', e => {
-  self.skipWaiting();
-});
-
+const CACHE = 'amicus-v5';
+self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.map(k => caches.delete(k)))
-    ).then(() => clients.claim())
-  );
+  e.waitUntil(caches.keys().then(k => Promise.all(k.map(c => caches.delete(c)))).then(() => clients.claim()));
 });
-
-// Network only - no caching
 self.addEventListener('fetch', e => {
-  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+  e.respondWith(fetch(e.request.url + '?v=5', {cache: 'no-store'}).catch(() => fetch(e.request)));
 });
